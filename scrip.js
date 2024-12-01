636677e47346cca1b9526562b33d6de50ea8c7b6
Faust <faustahza@gmail.com>
2024-12-01 10:49:19 +0700

    Javascript 1

---
+++ scrip.js
@@ -0,0 +1,16 @@
+console.log("Learning Javascript");
+
+let nama = "Faust";
+let school = "SMP";
+let age= "15";
+
+console.log(nama);
+console.log("Hey my name is " + nama);
+
+// / operasi aritmatika
+
+let x = 5;
+let y = 10;
+let z = x + y
+console.log(z)
+console.log(x + "+" + y + "=" + z)
